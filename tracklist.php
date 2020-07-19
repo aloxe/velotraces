9206271e901c97b75768e6177612c79ab38150ce
Alix Guillard <github@blog.re>
2020-07-19 20:59:29 +0200

    add country-year links

--- tracklist.php
+++ tracklist.php
@@ -40,6 +40,42 @@ while (($file = readdir($dir)) !== false) {
 closedir($dir);
 
 asort($tracks);
+$known_countries = array();
+$known_country_year = array();
+echo "<table class=\"countries\"><tr>";
+foreach ($tracks as $key => $val) {
+    $y = substr($val, 0, 4);
+    $countries = explode(".", $val);
+    $trackname = preg_replace('/[-_]/', ' ', substr($countries[0], 10));
+    array_shift($countries);
+    array_pop($countries);
+    foreach($countries as $k => $v) {
+        if (!in_array($v, $known_countries)) {
+            // convert iso 3166 letter to flag
+            $lettred = mb_convert_encoding('&#' . (ord(substr($v, 0, 1)) + 127462 - 97) . ';', 'UTF-8', 'HTML-ENTITIES');
+            $lettref = mb_convert_encoding('&#' . (ord(substr($v, 1, 2)) + 127462 - 97) . ';', 'UTF-8', 'HTML-ENTITIES');
+            echo "<th> <a href=\"./#?c=$v\" class=\"flag\">$lettred$lettref</a> </th>";
+            array_push($known_countries, $v);
+        }
+        if (!in_array($v.$y, $known_country_year)) {
+            array_push($known_country_year, $v.$y);
+        }
+    }
+}
+echo "</tr><tr>";
+foreach($known_countries as $k => $v) {
+    echo "<td valign=\"top\">";
+    foreach($known_country_year as $k2 => $v2) {
+        $v2y = substr($v2, 2,5);
+        if (substr($v2, 0,2) == $v) {
+            echo "<a href=\"./#?c=$v&y=$v2y\">$v2y</a><br/>";
+        }
+
+    }
+    echo "</td>";
+}
+echo "</tr></table>";
+
 echo "<table>";
 $i = 0;
 foreach ($tracks as $key => $val) {
