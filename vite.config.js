b1606b1271f53b5b2cb7a069513b57e933e9951b
aloxe <github@blog.re>
2024-04-25 08:45:59 +0200

    use axios and proxy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: 'https://alix.guillard.fr/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    },
+  },
   plugins: [react()],
-})
+});
