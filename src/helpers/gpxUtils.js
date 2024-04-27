f46f0b1a064aa03d3b25668b2fe8144d50320f5b
aloxe <github@blog.re>
2024-04-28 00:34:55 +0200

    populate popup

--- src/helpers/gpxUtils.js
+++ src/helpers/gpxUtils.js
@@ -5,7 +5,10 @@ export const getGeoJson = async (url) => {
   try {
     const response = await axios.get('/api/velotraces/allvelotracks/'+url);
     const xmlDom = new DOMParser().parseFromString(response.data, 'application/xml');
-    const geojson = gpx(xmlDom) // maybe use omnivore instead
+    const geojson = gpx(xmlDom) // maybe use omnivore instead?
+    geojson.date = getDate(url)
+    geojson.title = getTitle(url)
+    geojson.countries = getCountries(url)
     return geojson;
   } catch (error) {
     console.error('Error fetching gpx data:', error.message);
@@ -33,32 +36,4 @@ export const getTitle = (file) => {
   return title;
 }
 
-export const getCountries = (file) => {
-  const countries = [];
-  const array = file.split('.');
-  var i = 1;
-  while(i < array.length -1 ) {
-      array[i]
-      countries[i -1] = {
-          code: array[i],
-          name: getCountryName(array[i].toUpperCase())
-      }
-      countries[i -1].link = '<a href="#?c=' + countries[i -1].code + '">' + countries[i -1].name + "</a>";
-      countries[i -1].url = '#?c=' + countries[i -1].code;
-      i++;
-  }
-      countries[0].chain = ``;
-  for (var i = 0; i < countries.length; i++) {
-      if (countries[i] !== countries[0]) {
-          countries[0].chain += `, `;
-      }
-      if (countries[i]) {
-          countries[0].chain += `<a href="` + countries[i].url + `"   onClick="jumpTo('` + countries[i].url + `');">` + countries[i].name + `</a>`;
-      }
-  }
-  if (countries.length >= 1) {
-      return countries;
-  } else {
-      return false;
-  }
-}
+export const getCountries = (file) => file.split('.').slice(1, -1)
