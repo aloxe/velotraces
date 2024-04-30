bc6d66a696fa8e8c2c16ccb6afea23b6ca47f732
aloxe <github@blog.re>
2024-04-30 15:01:04 +0200

    install vite react leaflet

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: 'https://alix.guillard.fr/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    },
+  },
+  plugins: [react()],
+});
