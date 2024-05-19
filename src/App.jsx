03b263bbc47f6668b66fc0dd67736febc4bcaf79
aloxe <github@blog.re>
2024-05-19 23:18:10 +0200

    avoir rerender of sidebar (move up in wrapper)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import MainMap from './components/MainMap'
+import Wrapper from './components/Wrapper'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       {/* <h1>Plop</h1> */}
-    <MainMap />
+    <Wrapper />
     </div>
   )
 }
