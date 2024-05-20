a9e50e3ca6ae0447291373791aac70c234bfafb4
aloxe <github@blog.re>
2024-05-20 08:12:34 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      {/* <h1>Plop</h1> */}
-    <Wrapper />
+      <Wrapper />
     </div>
   )
 }
--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,24 +1,22 @@
 import {  useEffect, useState } from "react";
 import { Map, GeoJson } from "pigeon-maps"
-import Popup from "./Popup";
 import { CartoDBVoyager } from "../helpers/tiles";
 import { getCenter, getListBoundingBox, getZoom } from "../helpers/gpxUtil";
+import Popup from "./Popup";
 import './MainMap.css'
 
-const MainMap = ({geojson, geojsonList}) => {
+const MainMap = ({geojsonList}) => {
   const [center, setCenter] = useState({lon:3, lat:50});
   const [zoom, setZoom] = useState(8);
 
+  const [geojson, setGeojson] = useState(null);
   const [currentFocus, setCurrentFocus] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect");
     if (geojsonList.length) {
-      console.log("useEffect", geojsonList.length);
       const bbox = getListBoundingBox(geojsonList)
       setCenter(getCenter(bbox))
       setZoom(getZoom(bbox))
-      console.log("bbox", bbox);
     }
   }, [geojsonList]);
 
@@ -48,7 +46,7 @@ const MainMap = ({geojson, geojsonList}) => {
     );
   };
 
-  const handleClick = (e) => {
+  const handleClick = (e,geojson) => {
     if (currentFocus) {
       currentFocus.map(el => {
         el.setAttribute('stroke', 'red');
@@ -56,27 +54,25 @@ const MainMap = ({geojson, geojsonList}) => {
       })
     }
     const svgPathArray = [...e.event.target.parentNode.children];
+    // update track node
     setCurrentFocus(svgPathArray)
-    // setGeojson(geojson)
+    // update track data
+    setGeojson(geojson)
     svgPathArray.map(el => {
       el.setAttribute('stroke', 'blue');
       el.setAttribute('opacity', '1');
     })
 
-    const popEl = e.event.target.parentNode.parentNode.parentNode.parentNode.parentNode.children[1].children[2]
+    const popEl = e.event.target.parentNode.parentNode.parentNode.parentNode.parentNode.children[1].children[0]
     popEl.style.display = 'block';
-    // bellow we don't attach the popup to the trace
-    // as it would move with the map (FIXME)
+
+    // we don't attach the popup to the trace
+    // as it would move with the map
+    // TODO make it follow the track
     // popEl.style.left = e.event.clientX - 99+'px'
     // popEl.style.top = e.event.clientY - 108+'px'
-
-    // popEl.children[1].children[0].children[0].innerText = "🚲 " + geojson.title
-    // popEl.children[1].children[0].children[1].innerText = formatDate(undefined, "ddMMYYYY", geojson.date)
-    // popEl.children[1].children[0].children[2].innerText = geojson.countries.map(cc => countryCodeToFlag(cc)).join(' ')
-    // popEl.children[1].children[0].children[3].innerText = geojson.distance+'km' || '';
   }
 
-  console.log("RENDER MAP");
   return (
     <div className="MapWrapper">
       <Map
@@ -87,6 +83,7 @@ const MainMap = ({geojson, geojsonList}) => {
         center={[center.lat || 44, center.lon || 3]}
         zoom={zoom || 8}
       >
+        {/* {<>{geojson?.url}</>} */}
         {currentFocus && <Popup key='popup' currentFocus={currentFocus} geojson={geojson} />}
         { geojson && renderGeoJson(geojson, 'prems') }
         { geojsonList && geojsonList.length>=1 && geojsonList.map((json, i) => renderGeoJson(json, i)) }
