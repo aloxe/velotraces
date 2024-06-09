259e372bd4fcd67efa181b31fad8862af099313c
aloxe <github@blog.re>
2024-06-09 22:25:15 +0200

    prepare to upload files
    upload all geojson

---
+++ src/helpers/strings.js
@@ -0,0 +1,19 @@
+// Ascii language manipulation 
+const sansAccent = (str) => {
+    if (!str) return null;
+    str = str.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+    str = str.replace("œ", "oe");
+    str = str.replace("æ", "ae");
+    str = str.replace("Œ", "OE");
+    str = str.replace("Æ", "AE");
+    str = str.replace("ß", "ss");
+    str = str.replace("ẞ", "SS");
+    return str;
+  }
+
+  export const toSlug = (str) => {
+    return sansAccent(str).toLowerCase()
+      .replace(/[ ._']/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/-[-]+/g, "-");
+  }
