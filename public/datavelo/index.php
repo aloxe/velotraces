0569ee8b9049846beb74e55d9c78fac12ec126fb
aloxe <github@blog.re>
2024-09-24 20:59:06 +0200

    remove matomo

--- public/datavelo/index.php
+++ public/datavelo/index.php
@@ -102,23 +102,5 @@ foreach ($tracks as $key => $val) {
 }
 echo "</table>";
 ?>
-<!-- Matomo -->
-<script type="text/javascript">
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["setDocumentTitle", document.title]);
-  _paq.push(["setDomains", ["*.alix.guillard.fr","*.alix.guillard.fr"]]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//reunionweb.org/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//alix.guillard.fr/matomo/matomo.php?idsite=3&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Matomo Code -->
     </body>
 </html>
